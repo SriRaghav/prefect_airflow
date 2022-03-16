bae431418ad1a3b0bf36c24c145d89ea95e584fc
Raghavendiran Nagarajan <rnagarajan@apple.com>
2022-03-16 03:58:32 -0700

    Hello World

---
+++ prefect/hello_world.py
@@ -0,0 +1,40 @@
+
+from datasets import list_datasets, load_dataset, list_metrics, load_metric
+import prefect
+from prefect import task, Flow, Parameter
+from prefect.triggers import manual_only
+from prefect.engine.executors import LocalDaskExecutor
+
+@task
+def first_task(x):
+    print("Printing First task name: " + x)
+
+@task
+def second_task(x):
+    print("Printing the second task:" + x)
+
+@task
+def third_task(x):
+    print("Printing the third task:" + x)
+
+with Flow("SeqFlow") as flow:
+
+    first_task("no_1")
+    second_task("no_2")
+    third_task("no_3")
+
+
+'''def prefect_flow():
+
+    with Flow("SeqFlow") as flow:
+
+        first_task("no_1")
+        second_task("no_2")
+        third_task("no_3")
+    return flow'''
+
+
+if __name__=='__main__':
+    #flow = prefect_flow()
+    flow.run()
+
